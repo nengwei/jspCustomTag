559f4ba517ae580f6395f977a2927641c7111d70
neng.wei <1009002925@qq.com>
2017-06-22 23:03:30 +0800

    自定义时间tag

---
+++ jspCustomTag/src/com/custom/tag/LoginDateTag.java
@@ -0,0 +1,29 @@
+package com.custom.tag;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * 自定义时间标签在页面显示
+ * 
+ * @author weineng
+ *
+ */
+public class LoginDateTag extends TagSupport {
+
+	public int doStartTag() throws JspException {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		String dateStr = simpleDateFormat.format(new Date());
+		try {
+			pageContext.getOut().print(dateStr);//
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return super.doStartTag();
+	}
+
+}
